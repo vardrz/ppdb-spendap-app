d736de1e13506a0f7a59b1a617ecf75a3315aaeb
ikkromm18 <muhammadikkrom749@gmail.com>
2024-07-26 10:27:39 +0700

    feat: tambahan kecil

--- lib/admin/data_pesertadidik.dart
+++ lib/admin/data_pesertadidik.dart
@@ -12,7 +12,38 @@ class _DataPesertadidikState extends State<DataPesertadidik> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Data Peserta Didik"),
+        backgroundColor: Colors.green[700],
+        centerTitle: true,
+        title: Text(
+          "Data Peserta Didik Baru",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 24, color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "Peserta Didik Tahun 2024/2025",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Center(
+              child: Text(
+                "Tabel Peserta Didik Baru Yang Telah Diverifikasi",
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+          ],
+        ),
       ),
     );
   }
